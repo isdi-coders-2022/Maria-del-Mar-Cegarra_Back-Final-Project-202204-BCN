9e8498ac47a58108f50f3e94746ae306075e4484
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-07 23:52:29 +0200

    Created createPost controller

--- src/server/controllers/postControllers/postControllers.js
+++ src/server/controllers/postControllers/postControllers.js
@@ -1,5 +1,7 @@
 const debug = require("debug")("set-appArt:server:postControllers");
 const chalk = require("chalk");
+const fs = require("fs");
+const path = require("path");
 const Post = require("../../../database/models/Post");
 
 const getPosts = async (req, res, next) => {
@@ -50,4 +52,46 @@ const deletePost = async (req, res, next) => {
   }
 };
 
-module.exports = { getPosts, deletePost };
+const createPost = async (req, res, next) => {
+  try {
+    const { caption, userId, hashtags, gallery } = req.body;
+    const date = Date.now();
+    const newPost = {
+      user: userId,
+      caption,
+      date,
+      hashtags,
+      gallery,
+    };
+
+    const { file } = req;
+    if (file) {
+      const newFileName = `${file.originalname.split(".")[0]}-${Date.now()}.${
+        file.originalname.split(".")[1]
+      }`;
+      fs.rename(
+        path.join("uploads", "images", file.filename),
+        path.join("uploads", "images", newFileName),
+        (error) => {
+          if (error) {
+            debug(chalk.red("Error renaming image of project"));
+
+            next(error);
+          }
+        }
+      );
+      newPost.picture = newFileName;
+    }
+    const createdPost = await Post.create(newPost);
+
+    res.status(201).json({ post: createdPost });
+  } catch (error) {
+    error.statusCode = 409;
+    error.customMessage = "Couldn't create project";
+    debug(chalk.red(error.message));
+
+    next(error);
+  }
+};
+
+module.exports = { getPosts, deletePost, createPost };
