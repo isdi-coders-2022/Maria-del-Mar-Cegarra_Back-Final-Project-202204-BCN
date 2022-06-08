6596d9a8de7d2af789a1d0896fa135e96504b914
Maria del Mar <maria.cegarra2303@gmail.com>
2022-06-08 16:54:21 +0200

    Added connnetion to firebase in createPost

--- src/server/controllers/postControllers/postControllers.js
+++ src/server/controllers/postControllers/postControllers.js
@@ -1,6 +1,13 @@
 const debug = require("debug")("set-appArt:server:postControllers");
 const chalk = require("chalk");
 const fs = require("fs");
+const { initializeApp } = require("firebase/app");
+const {
+  getStorage,
+  uploadBytes,
+  getDownloadURL,
+  ref,
+} = require("firebase/storage");
 const path = require("path");
 const Post = require("../../../database/models/Post");
 
@@ -53,38 +60,71 @@ const deletePost = async (req, res, next) => {
 };
 
 const createPost = async (req, res, next) => {
-  try {
-    const { caption, userId, hashtags, gallery } = req.body;
-    const date = Date.now();
-    const newPost = {
-      user: userId,
-      caption,
-      date,
-      hashtags,
-      gallery,
-    };
+  const { caption, userId, hashtags, gallery } = req.body;
+  const date = Date.now();
 
-    const { file } = req;
+  const newPost = {
+    user: userId,
+    caption,
+    date,
+    hashtags,
+    gallery,
+  };
+  const { file } = req;
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBiiSAjQY776X5a0wd4opeJ1oWRankL4O4",
+    authDomain: "pictures-9465d.firebaseapp.com",
+    projectId: "pictures-9465d",
+    storageBucket: "pictures-9465d.appspot.com",
+    messagingSenderId: "458777295431",
+    appId: "1:458777295431:web:f31652e5caeda301610f62",
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  try {
     if (file) {
       const newFileName = `${file.originalname.split(".")[0]}-${Date.now()}.${
         file.originalname.split(".")[1]
       }`;
+
       fs.rename(
         path.join("uploads", "images", file.filename),
         path.join("uploads", "images", newFileName),
-        (error) => {
+        async (error) => {
           if (error) {
-            debug(chalk.red("Error renaming picture post"));
+            debug(chalk.red("Error renaming post picture"));
 
             next(error);
+            return;
           }
+          newPost.picture = newFileName;
+
+          fs.readFile(
+            path.join("uploads", "images", newFileName),
+            async (readError, readFile) => {
+              if (readError) {
+                debug(chalk.red("Error reading post picture"));
+
+                next(readError);
+                return;
+              }
+              const storage = getStorage(firebaseApp);
+
+              const storageRef = ref(storage, newFileName);
+              await uploadBytes(storageRef, readFile);
+              const firebaseFileURL = await getDownloadURL(storageRef);
+
+              newPost.pictureBackup = firebaseFileURL;
+
+              const createdPost = await Post.create(newPost);
+              res.status(201).json({ post: createdPost });
+            }
+          );
         }
       );
-      newPost.picture = newFileName;
     }
-    const createdPost = await Post.create(newPost);
-
-    res.status(201).json({ post: createdPost });
   } catch (error) {
     error.statusCode = 409;
     error.customMessage = "Couldn't create post";
