c093b93913f16f4985f43628c4921896227c4643
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-06-13 02:23:30 +0200

    Added firebase and filestore to register endpoint

--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -5,12 +5,20 @@ const path = require("path");
 const {
   registerUser,
   loginUser,
+  getUser,
 } = require("../controllers/userControllers/userControllers");
+const firebase = require("../middlewares/firebase/firebase");
 
 const userRouter = express.Router();
 const uploads = multer({ dest: path.join("uploads", "images") });
 
-userRouter.post("/register", uploads.single("image"), registerUser);
+userRouter.post(
+  "/register",
+  uploads.single("profilePic"),
+  firebase,
+  registerUser
+);
 userRouter.post("/login", loginUser);
+userRouter.get("/user/:userId", getUser);
 
 module.exports = userRouter;
